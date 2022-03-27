02ade1d6429fd6be300f7a5621b238076f030f74
niknal357 <46793548+niknal357@users.noreply.github.com>
2022-03-27 14:01:56 +0200

    Update quiqbot.py

--- quiqbot.py
+++ quiqbot.py
@@ -16,21 +16,21 @@ def bot(grid, playing_as):
     if playing_as == 'x':
         opponent = 'o'
     lines = [
-        list('---xx_xx-'),
-        list('--xxx_x--'),
-        list('-xxxx_---'),
-        list('--xoo_oo-'),
-        list('-xooo_o--'),
-        list('xoooo_---'),
-        list('noooo_---'),
-        list('x-ooo_o--'),
-        list('--ooo_o--'),
-        list('-_xxx_---'),
-        list('--_xx_x_-'),
-        list('-_ooo_---'),
-        list('--_oo_o_-'),
-        list('---oo_o_-'),
-        list('--_oo_o--'),
+        to_line_3('---xx_xx-'),
+        to_line_3('--xxx_x--'),
+        to_line_3('-xxxx_---'),
+        to_line_3('--xoo_oo-'),
+        to_line_3('-xooo_o--'),
+        to_line_3('xoooo_---'),
+        to_line_3('noooo_---'),
+        to_line_3('x-ooo_o--'),
+        to_line_3('--ooo_o--'),
+        to_line_3('-_xxx_---'),
+        to_line_3('--_xx_x_-'),
+        to_line_3('-_ooo_---'),
+        to_line_3('--_oo_o_-'),
+        to_line_3('---oo_o_-'),
+        to_line_3('--_oo_o--'),
     ]
     for line_to_defo in lines:
         for pos in get_possible_positions(grid):
@@ -55,20 +55,71 @@ def bot(grid, playing_as):
         yield quiq
     yield
     yield random.choice(get_inflated_pos(grid))
-def to_line_3(string):
-    out = []
-    for char in string:
-        out.append(char)
-    return out
-
-
 def get_of_grid_3(grid, pos):
     if pos[0] < 0 or pos[0] > len(grid)-1 or pos[1] < 0 or pos[1] > len(grid[0])-1:
         return 'n'
     else:
         return grid[pos[0]][pos[1]]
+def invertDir(dir):
+    return (-dir[0], -dir[1])
 
 
+def dirToStr(dir):
+    return str(dir[0])+'x'+str(dir[1])
+def Quiqfinder(grid, placing_as):
+    patterns = [
+        #      placing: V
+        to_line_3('-_xxx_o--'),
+        to_line_3('--_xx_xo-'),
+        to_line_3('---_x_xxo'),
+        to_line_3('-oxxx__--'),
+        to_line_3('-x_xx_o--'),
+        to_line_3('--x_x_xo-'),
+        to_line_3('---x__xxo'),
+        to_line_3('oxxx__---'),
+        to_line_3('-xx_x_o--'),
+        to_line_3('--xx__xo-'),
+        to_line_3('-oxx__x--'),
+        to_line_3('oxx_x_---'),
+        to_line_3('-__xx___-'),
+        to_line_3('--__x_x__'),
+        to_line_3('-_x_x__--'),
+        to_line_3('--_x__x_-'),
+        to_line_3('-_xx___--'),
+        # to_line_3('_x_x___--'),
+        # to_line_3('--_x___x_'),
+        # to_line_3('_xx____--'),
+        # to_line_3('_x__x__--'),
+        # to_line_3('-_x___x_-'),
+    ]
+    #x_s = list(range(len(grid)))
+    #y_s = list(range(len(grid[0])))
+    # random.shuffle(x_s)
+    # random.shuffle(y_s)
+    # for x in x_s:
+    #    for y in y_s:
+    for pos in get_inflated_pos(grid):
+        x, y = pos
+        matched = {}
+        for dir in ALLDIRS:
+            matched[dirToStr(dir)] = None
+        cnt = 0
+        for dir in ALLDIRS:
+            line = get_line_3(
+                grid, (x-dir[0]*5, y-dir[1]*5), 9, dir, placing_as == 'o')
+            for i, pattern in enumerate(patterns):
+                if matched[dirToStr(invertDir(dir))] != i:
+                    if intersect_lines(pattern, line):
+                        matched[dirToStr(dir)] = i
+                        cnt += 1
+                        if cnt >= 2:
+                            return (x, y)
+    return None
+def to_line_3(string):
+    out = []
+    for char in string:
+        out.append(char)
+    return out
 def get_line_3(grid, pos, dist, dir, invert=False):
     out = []
     for i in range(dist):
@@ -93,101 +144,33 @@ def intersect_lines(l1, l2):
         if l1[i] != '-' and l2[i] != '-' and l1[i] != l2[i]:
             return False
     return True
-    
-def get_possible_positions(grid):
-    GRID_SIZE_X = len(grid)
-    GRID_SIZE_Y = len(grid[0])
-    possible_positions = []
-    for x in range(GRID_SIZE_X):
-        for y in range(GRID_SIZE_Y):
-            if grid[x][y] != '_':
-                left_good = False
-                right_good = False
-                up_good = False
-                down_good = False
-                if x < GRID_SIZE_X-1:
-                    right_good = True
-                if x > 0:
-                    left_good = True
-                if y < GRID_SIZE_Y-1:
-                    down_good = True
-                if y > 0:
-                    up_good = True
-                if up_good and left_good and grid[x-1][y-1] == '_':
-                    if (x-1, y-1) not in possible_positions:
-                        possible_positions.append((x-1, y-1))
-                if up_good and grid[x][y-1] == '_':
-                    if (x, y-1) not in possible_positions:
-                        possible_positions.append((x, y-1))
-                if right_good and up_good and grid[x+1][y-1] == '_':
-                    if (x+1, y-1) not in possible_positions:
-                        possible_positions.append((x+1, y-1))
-                if left_good and grid[x-1][y] == '_':
-                    if (x-1, y) not in possible_positions:
-                        possible_positions.append((x-1, y))
-                if right_good and grid[x+1][y] == '_':
-                    if (x+1, y) not in possible_positions:
-                        possible_positions.append((x+1, y))
-                if down_good and left_good and grid[x-1][y+1] == '_':
-                    if (x+1, y+1) not in possible_positions:
-                        possible_positions.append((x-1, y+1))
-                if down_good and grid[x][y+1] == '_':
-                    if (x, y+1) not in possible_positions:
-                        possible_positions.append((x, y+1))
-                if right_good and down_good and grid[x+1][y+1] == '_':
-                    if (x+1, y+1) not in possible_positions:
-                        possible_positions.append((x+1, y+1))
-    if len(possible_positions) == 0:
-        return [(GRID_SIZE_X//2, GRID_SIZE_Y//2)]
-    return possible_positions
-def Quiqfinder(grid, placing_as):
-    patterns = [
-        # placing: V
-        list('-_xxx_---'),
-        list('--_xx_x--'),
-        list('---_x_xx-'),
-        list('--xxx__--'),
-        list('-x_xx_---'),
-        list('--x_x_x--'),
-        list('---x__xx-'),
-        list('-xxx__---'),
-        list('-xx_x_---'),
-        list('--xx__x--'),
-        list('--xx__x--'),
-        list('-xx_x_---'),
-        list('-__xx___-'),
-        list('--__x_x__'),
-        list('-_x_x__--'),
-        list('--_x__x_-'),
-        list('-_xx___--'),
-    ]
-    #x_s = list(range(len(grid)))
-    #y_s = list(range(len(grid[0])))
-    # random.shuffle(x_s)
-    # random.shuffle(y_s)
-    # for x in x_s:
-    #    for y in y_s:
-    for pos in get_inflated_pos(grid):
-        x, y = pos
-        dirs_done = []
-        cnt = 0
-        for dir in ALLDIRS:
-            line = get_line_3(
-                grid, (x-dir[0]*5, y-dir[1]*5), 9, dir, placing_as == 'o')
-            for i, pattern in enumerate(patterns):
-                if invertDir(dir) in dirs_done:
-                    continue
-                if intersect_lines(pattern, line):
-                    dirs_done.append(dir)
-                    cnt += 1
-                    if cnt >= 2:
-                        return (x, y)
-    return None
-def get_of_grid_3(grid, pos):
-    if pos[0] < 0 or pos[0] > len(grid)-1 or pos[1] < 0 or pos[1] > len(grid[0])-1:
-        return 'n'
-    else:
-        return grid[pos[0]][pos[1]]
+def get_inflated_pos(grid):
+    out = []
+    for x in range(len(grid)):
+        for y in range(len(grid[0])):
+            if get_of_grid_3(grid, (x, y)) != '_':
+                continue
+            for offset in INFLATED_OFFSETS:
+                x_offset = offset[0]
+                y_offset = offset[1]
+                #print(x+x_offset, y+y_offset)
+                if get_of_grid_3(grid, (x+x_offset, y+y_offset)) in ['x', 'o']:
+                    out.append((x, y))
+                    break
+            # for x_offset in range(-2, 3):
+            #    for y_offset in range(-2, 3):
+            #        if x_offset == 0 and y_offset == 0:
+            #            continue
+            #        if get_of_grid_3(grid, (x+x_offset, y+y_offset)) in ['x', 'o']:
+            #            out.append((x, y))
+            #            done = True
+            #            break
+            #    if done:
+            #        break
+    # print(out)
+    if len(out) == 0:
+        out.append((len(grid)//2, len(grid[0])//2))
+    return out
 def get_possible_positions(grid):
     possible_positions = []
     for x in range(len(grid)):
@@ -232,33 +215,3 @@ def get_possible_positions(grid):
     if len(possible_positions) == 0:
         return [(len(grid)//2, len(grid[0])//2)]
     return possible_positions
-
-def invertDir(dir):
-    return (-dir[0], -dir[1])
-def get_inflated_pos(grid):
-    out = []
-    for x in range(len(grid)):
-        for y in range(len(grid[0])):
-            if get_of_grid_3(grid, (x, y)) != '_':
-                continue
-            for offset in INFLATED_OFFSETS:
-                x_offset = offset[0]
-                y_offset = offset[1]
-                #print(x+x_offset, y+y_offset)
-                if get_of_grid_3(grid, (x+x_offset, y+y_offset)) in ['x', 'o']:
-                    out.append((x, y))
-                    break
-            # for x_offset in range(-2, 3):
-            #    for y_offset in range(-2, 3):
-            #        if x_offset == 0 and y_offset == 0:
-            #            continue
-            #        if get_of_grid_3(grid, (x+x_offset, y+y_offset)) in ['x', 'o']:
-            #            out.append((x, y))
-            #            done = True
-            #            break
-            #    if done:
-            #        break
-    # print(out)
-    if len(out) == 0:
-        out.append((len(grid)//2, len(grid[0])//2))
-    return out
