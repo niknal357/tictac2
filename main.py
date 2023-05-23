054b23958aac610df913cb562269b0666a23f11f
niknal357 <46793548+niknal357@users.noreply.github.com>
2023-05-23 21:11:17 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -13,24 +13,72 @@ def module_from_file(module_name, file_path):
     return module
 
 
-if not os.path.exists('mods/'):
-    os.makedirs('mods/')
+def read_params(filename):
+    extension = filename.split('.')[-1]
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+    params = {}
+    if extension == 'py':
+        comment = '#'
+    elif extension == 'bat':
+        comment = '::'
+    else:
+        print('Unknown extension: ' + extension)
+        return params
+    for lin in lines:
+        line = lin.strip()
+        if line == '':
+            continue
+        if not line.startswith(comment):
+            break
+        line = line.split(comment)[1:]
+        line = comment.join(line)
+        if ':' not in line:
+            break
+        values = line.split(':')
+        if len(values) != 2:
+            continue
+        params[values[0].strip()] = values[1].strip()
+    return params
 
-subprocess.run(['curl', 'https://raw.githubusercontent.com/niknal357/tictac2/main/bot_reference.json', '-o', 'bot_reference.json'], shell=True)
-with open('bot_reference.json', 'r') as f:
-    bots_to_download = json.loads(f.read())
-for bot_to_download in bots_to_download:
-    if bot_to_download['file'] not in os.listdir('mods/'):
-        subprocess.run(['curl', bot_to_download['url'], '-o', 'mods/'+bot_to_download['file']])
+
+if not os.path.exists('plugins/'):
+    os.makedirs('plugins/')
+
+subprocess.run(['curl', '-H', "'Cache-Control: no-cache, no-store'", 'https://raw.githubusercontent.com/niknal357/tictac2/main/file_reference.json',
+               '-o', 'file_reference.json'], shell=True)
+
+with open('file_reference.json', 'r') as f:
+    files = json.loads(f.read())
+to_dl = []
+for filedata in files:
+    if not os.path.exists(filedata['file']):
+        to_dl.append(filedata)
+        continue
+    params = read_params(filedata['file'])
+    if 'version' not in params:
+        to_dl.append(filedata)
+        continue
+    if params['version'] != filedata['version']:
+        to_dl.append(filedata)
+        continue
+for filedata in to_dl:
+    print('Downloading ' + filedata['file'] + '...')
+    subprocess.run(['curl', '-H', "'Cache-Control: no-cache, no-store'", filedata['url'], '-o',
+                   filedata['file']], shell=True)
+
+for filedata in to_dl:
+    print('Downloading ' + filedata['file'] + '...')
 
 launcher = None
 engine = None
 bots = [{'name': 'Human', 'func': None}]
 
-for mod in os.listdir('mods'):
+
+for mod in os.listdir('plugins'):
     if mod.split('.')[-1] != 'py':
         continue
-    with open('mods/' + mod, 'r') as f:
+    with open('plugins/' + mod, 'r') as f:
         lines = f.readlines()
     firstline = lines[0].strip('#').strip()
     if firstline.split(':')[0].strip() == 'mod_type':
@@ -39,10 +87,10 @@ for mod in os.listdir('mods'):
         continue
     if mod_type == 'launcher':
         launcher = module_from_file(
-            mod, 'mods/'+mod).launcher
+            mod, 'plugins/'+mod).launcher
     elif mod_type == 'engine':
         engine = module_from_file(
-            mod, 'mods/'+mod).engine
+            mod, 'plugins/'+mod).engine
     elif mod_type == 'bot':
         secondline = lines[1].strip('#').strip()
         if secondline.split(':')[0].strip() == 'bot_name':
@@ -50,31 +98,33 @@ for mod in os.listdir('mods'):
         else:
             continue
         bots.append({'name': bot_name, 'func': module_from_file(
-            mod, 'mods/'+mod).bot})
+            mod, 'plugins/'+mod).bot})
 
 if launcher == None:
     print('No launcher found, downloading default...')
-    subprocess.run(['curl', 'https://raw.githubusercontent.com/niknal357/tictac2/main/default_launcher.py', '-o', 'mods/default_launcher.py'], shell=True)
+    subprocess.run(['curl', 'https://raw.githubusercontent.com/niknal357/tictac2/main/default_launcher.py',
+                   '-o', 'plugins/default_launcher.py'], shell=True)
     launcher = module_from_file(
-        'default_launcher.py', 'mods/default_launcher.py').launcher
+        'default_launcher.py', 'plugins/default_launcher.py').launcher
 if engine == None:
     print('No engine found, downloading default...')
-    subprocess.run(['curl', 'https://raw.githubusercontent.com/niknal357/tictac2/main/default_engine.py', '-o', 'mods/default_engine.py'], shell=True)
+    subprocess.run(['curl', 'https://raw.githubusercontent.com/niknal357/tictac2/main/default_engine.py',
+                   '-o', 'plugins/default_engine.py'], shell=True)
     engine = module_from_file(
-        'default_engine.py', 'mods/default_engine.py').engine
+        'default_engine.py', 'plugins/default_engine.py').engine
 
-#if True:
+# if True:
 #    print('You seem to have no bots installed.')
 #    #inp = input('would you like to install the default set of bots? (y/n) ')
 #    if True:
-#        subprocess.run(['curl', 'https://raw.githubusercontent.com/niknal357/tictac2/main/avajaris.py', '-o', 'mods/avajaris.py'], shell=True)
-#        subprocess.run(['curl', 'https://raw.githubusercontent.com/niknal357/tictac2/main/bot_3.py', '-o', 'mods/bot_3.py'], shell=True)
-#        subprocess.run(['curl', 'https://raw.githubusercontent.com/niknal357/tictac2/main/bot_7.py', '-o', 'mods/bot_7.py'], shell=True)
-#        subprocess.run(['curl', 'https://raw.githubusercontent.com/niknal357/tictac2/main/bot_8.py', '-o', 'mods/bot_8.py'], shell=True)
-#        for mod in os.listdir('mods'):
-#            if mod.split('.')[-1] != 'py':  
+#        subprocess.run(['curl', 'https://raw.githubusercontent.com/niknal357/tictac2/main/avajaris.py', '-o', 'plugins/avajaris.py'], shell=True)
+#        subprocess.run(['curl', 'https://raw.githubusercontent.com/niknal357/tictac2/main/bot_3.py', '-o', 'plugins/bot_3.py'], shell=True)
+#        subprocess.run(['curl', 'https://raw.githubusercontent.com/niknal357/tictac2/main/bot_7.py', '-o', 'plugins/bot_7.py'], shell=True)
+#        subprocess.run(['curl', 'https://raw.githubusercontent.com/niknal357/tictac2/main/bot_8.py', '-o', 'plugins/bot_8.py'], shell=True)
+#        for mod in os.listdir('plugins'):
+#            if mod.split('.')[-1] != 'py':
 #                continue
-#            with open('mods/' + mod, 'r') as f:
+#            with open('plugins/' + mod, 'r') as f:
 #                lines = f.readlines()
 #            firstline = lines[0].strip('#').strip()
 #            if firstline.split(':')[0].strip() == 'mod_type':
@@ -88,7 +138,7 @@ if engine == None:
 #                else:
 #                    continue
 #                bots.append({'name': bot_name, 'func': module_from_file(
-#                    mod, 'mods/'+mod).bot})
+#                    mod, 'plugins/'+mod).bot})
 
 
 while True:
