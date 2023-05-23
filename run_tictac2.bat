3649bb1dbac8900bcb1b327052e381b847ac80a7
niknal357 <46793548+niknal357@users.noreply.github.com>
2023-05-23 21:15:38 +0200

    added cache control headers to batch launcher

--- run_tictac2.bat
+++ run_tictac2.bat
@@ -1,15 +1,15 @@
-:: version: 1.0.0
+:: version: 1.0.1
 @echo off
 python --version > NUL
 if ERRORLEVEL 1 GOTO NOPYTHON
 goto :HASPYTHON
 :NOPYTHON
 echo Installing python, please wait...
-curl --insecure https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe -o python_installer.exe
+curl -H 'Cache-Control: no-cache, no-store' --insecure https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe -o python_installer.exe
 python_installer.exe /passive InstallAllUsers=1 PrependPath=1
 set PATH=%PATH%,C:\Program Files\Python310\python.exe
 :HASPYTHON
-curl --insecure https://raw.githubusercontent.com/niknal357/tictac2/main/main.py -o main.py
+curl -H 'Cache-Control: no-cache, no-store' --insecure https://raw.githubusercontent.com/niknal357/tictac2/main/main.py -o main.py
 python main.py
 IF ERRORLEVEL 1 GOTO HASPYTHON
 :end
